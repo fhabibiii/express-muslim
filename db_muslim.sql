3fcf5b2a4853ac10bd99a4f6eeec348af79ab2cd
fhabibiii <faqihuddinh217@gmail.com>
2025-01-05 15:50:57 +0700

    db

--- db_muslim.sql
+++ db_muslim.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 05, 2025 at 05:12 AM
+-- Generation Time: Jan 05, 2025 at 05:10 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_sholatku`
+-- Database: `db_muslim`
 --
 
 -- --------------------------------------------------------
